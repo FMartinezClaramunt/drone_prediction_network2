78b66da5c191e3afaf049bba361ef17d3e8ee8d2
Hai Zhu <zhuhai.frank@gmail.com>
2020-06-26 21:11:22 +0200

    Add path prediction server

--- src/prediction_node.py
+++ src/prediction_node.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python
 import time
 import rospy
-import sys;
+import sys
 print('Python %s on %s' % (sys.version, sys.platform))
 import os
+import numpy as np
 
 os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 from drone_prediction_network2.srv import MultiTrajPrediction, MultiTrajPredictionResponse
 
 # Import model
-from src.predict import trajectory_predictor
+from predict import trajectory_predictor
 
-n_robots = 1
-n_obstacles = 0
+n_robots = 4
+n_obstacles = 2
 prediction_network = trajectory_predictor(n_robots, n_obstacles)
 
 
 def query_network(req):
-	resp = MultiTrajPredictionResponse()
+	
 	robot_data = req.robotTrajHistory.data
+	robot_data = np.reshape(robot_data, (6, prediction_network.past_horizon, prediction_network.n_robots), order="F")
 	obstacle_data = req.obsState.data
+	obstacle_data = np.reshape(obstacle_data, (6, prediction_network.n_obstacles), order="F")
 	predicted_trajectories = prediction_network.predict(robot_data, obstacle_data)
 
-	resp.data = predicted_trajectories
+	resp = MultiTrajPredictionResponse()
+	resp.robotTrajPrediction.data = np.reshape(predicted_trajectories, (3*prediction_network.prediction_horizon*prediction_network.n_robots, 1), order="F")
 	return resp
 
 
 if __name__ == '__main__':
-	rospy.init_node('Drone_Prediction_node')
+	rospy.init_node('drone_prediction_server')
 
-	s = rospy.Service('/predict', MultiTrajPrediction, query_network)
+	s = rospy.Service('/drone_trajectory_prediction', MultiTrajPrediction, query_network)
 	print("Ready to provide drone predictions.")
 	rospy.spin()
