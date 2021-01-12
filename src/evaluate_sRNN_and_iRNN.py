9647e3c3095a3a615ccef5b879b67b780e8cd693
Francisco Martinez Claramunt <FMartinezClaramunt@outlook.com>
2021-01-12 22:26:31 +0100

    Fix issue with scaler

--- src/evaluate_sRNN_and_iRNN.py
+++ src/evaluate_sRNN_and_iRNN.py
@@ -18,7 +18,7 @@ import copy
 # dataset_name = "cent20_10obs_log_20210106_233859"
 
 datasets = ["randomCentralized_noObs", "randomCentralized_dynObs", "randomCentralized_noObs4", "randomCentralized_dynObs4"]
-prefix = "results10d_"
+prefix = "replicate_"
 
 iRNN_model_name = "dynamicEllipsoidObstaclesRNN_commonInputMaxPooling_alt"
 iRNN_model_number = "508"
@@ -46,7 +46,7 @@ for dataset_name in datasets:
     args.past_horizon = past_horizon
 
     assert "scaler" in args
-    scaler = args.scaler
+    iRNN_scaler = args.scaler
 
     model_iRNN = model_selector(args)
 
@@ -61,7 +61,7 @@ for dataset_name in datasets:
     args.past_horizon = past_horizon
 
     assert "scaler" in args
-    scaler = args.scaler
+    sRNN_scaler = args.scaler
 
     model_sRNN = model_selector(args)
     
@@ -176,15 +176,16 @@ for dataset_name in datasets:
     del mpc_plan
 
     print("Obtaining predictions for iRNN")
-    scaled_data = scaler.transform(input_data)
-    scaled_data["target"] = model_iRNN.predict(scaled_data)
-    vel_prediction_iRNN = scaler.inverse_transform({"target": scaled_data["target"]})["target"]
+    iRNN_scaled_data = iRNN_scaler.transform(input_data)
+    iRNN_scaled_data["target"] = model_iRNN.predict(iRNN_scaled_data)
+    vel_prediction_iRNN = iRNN_scaler.inverse_transform({"target": iRNN_scaled_data["target"]})["target"]
 
     print("Obtaining predictions for sRNN")
-    scaled_data["target"] = model_sRNN.predict(scaled_data)
-    vel_prediction_sRNN = scaler.inverse_transform({"target": scaled_data["target"]})["target"]
+    sRNN_scaled_data = sRNN_scaler.transform(input_data)
+    sRNN_scaled_data["target"] = model_sRNN.predict(sRNN_scaled_data)
+    vel_prediction_sRNN = sRNN_scaler.inverse_transform({"target": sRNN_scaled_data["target"]})["target"]
 
-    del scaled_data, model_iRNN, model_sRNN
+    del iRNN_scaled_data, sRNN_scaled_data, model_iRNN, model_sRNN
 
     for step in range(1, prediction_horizon+1):
         pos_prediction_iRNN[:, step, :] = pos_prediction_iRNN[:, step-1, :] + dt * vel_prediction_iRNN[:, step-1, :]
